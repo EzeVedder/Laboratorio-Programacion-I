5a893538289da80fa7c0ee0e356c261ea2ac7eff
EzeVedder <pane.ezequiel@gmail.com>
2018-04-19 12:19:21 -0300

    Actualizado.MostrarAlumnos_

--- LAB-PROG I/Clases/Clase_07/main.c
+++ LAB-PROG I/Clases/Clase_07/main.c
@@ -3,17 +3,19 @@
 #define tam 3
 
 float calcularPromedio(int,int);
+void mostrarAlumnos(int[],char[][30],int[],int[],float[],int);
+
 
 int main()
 {
-    int legajo[3];//2 + el \0
-    char nombre[3][30];//matriz de 3x30 29+\0
-    int nota1[3];
-    int nota2[3];
-    float promedio[3];
+    int legajo[tam];//2 + el \0
+    char nombre[tam][30];//matriz de 3x30 29+\0
+    int nota1[tam];
+    int nota2[tam];
+    float promedio[tam];
     int i;
 
-    for(i=0;i<3; i++)
+    for(i=0;i<tam; i++)
     {
         printf("\nIngrese el legajo: ");
         scanf("%d",&legajo[i]);
@@ -30,18 +32,29 @@ int main()
         scanf("%d",&nota2[i]);
 
         promedio[i] = calcularPromedio(nota1[i],nota2[i]);
-        printf("El promedio del alumno %s, es: %.2f",nombre[i],promedio[i]);
 
     }
+        mostrarAlumnos(legajo,nombre,nota1,nota2,promedio,tam);
 
 
     return 0;
 }
 
 
-float calcularPromedio(nota1,nota2)
+float calcularPromedio(int nota1,int nota2)
 {
     float promedio;
     promedio = (float)(nota1 + nota2)/2;
     return promedio;
 }
+
+void mostrarAlumnos(int legajo[],char nombre[][30],int nota1[],int nota2[],float promedio[],int cantidad)
+{
+    int i;
+    for(i=0;i<cantidad;i++)
+    {
+        printf("\n%5d %15s %5d %5d %.2f\n",legajo[i],nombre[i],nota1[i],nota2[i],promedio[i]);
+    }
+}
+
+

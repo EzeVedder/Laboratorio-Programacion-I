715f4a39660b576d9cfbad80cc38b2669ac363a4
EzeVedder <pane.ezequiel@gmail.com>
2018-04-20 11:56:57 -0300

    ----------EJERCICIO INCOMPLETO-----------

--- LAB-PROG I/Clases/Clase_08_2parte/main.c
+++ LAB-PROG I/Clases/Clase_08_2parte/main.c
@@ -3,51 +3,81 @@
 #include <string.h>
 #define TAM 200
 
+int buscarLibre(int[],int);
+void mostrarAlumnos(int[],int);
+
+
 int main()
 {
-
-    char alumno[];
+    int legajo[TAM];
+    char alumnos[TAM][30];
+    int alumno[TAM];
     int opcion;
+    int posicion;
 
     printf("\tGestion de Alumnos\n");
     do
     {
-        printf("\n1.ALTA\n2.MOSTRAR\n3.BAJA\n4.ORDENAR\n5.SALIR\n");
+        printf("\n1.ALTA\n2.MOSTRAR\n3.BAJA\n4.MODIFICAR\n5,ORDENAR\n6.SALIR\n");
         printf("Ingrese una opcion: ");
         scanf("%d",&opcion);
 
-        switch(opcion);
+        switch(opcion)
         {
             case 1:
-            printf("Ingrese el nombre del alumno: ");
-            fflush(stdin);
-            gets(alumno);
-
-
+                posicion = buscarLibre(alumno,TAM);
+                if(posicion!=-1)
+                {
+                    printf("Ingrese legajo:");
+                    scanf("%d",&legajo[TAM]);
+                    printf("Ingrese el nombre del alumno: ");
+                    fflush(stdin);
+                    scanf("%[^\n]",alumnos[posicion]);
+                }
+                else
+                {
+                    printf("No hay mas espacio para ingresar datos. ");
+                }
             break;
-
             case 2:
+                printf("MOSTRAR\n");
             break;
 
             case 3:
+                printf("BAJA\n");
             break;
 
             case 4:
+                printf("MODIFICAR\n");
             break;
 
             case 5:
+                printf("ORDENAR\n");
             break;
 
+            case 6:
+                printf("SALIR\n");
+            break;
         }
-
 
     }while(opcion<=6);
 
-
-
-
-
-
-
     return 0;
+}
+
+int buscarLibre(int alumno[],int tam)
+{
+    int index = -1;
+    int i;
+    for(i=0;i<tam;i++)
+    {
+        if(alumno[i]==0)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+
 }
